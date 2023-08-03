9cdc3f831b65e16ba9bbb85b771b275eeeda9693
Deril Alwinda <deril.alwinda@harrisma.com>
2023-08-04 00:29:19 +0700

    4 Agustus 2023

--- bbs/webapp/controller/materialIssue/Detail.controller.js
+++ bbs/webapp/controller/materialIssue/Detail.controller.js
@@ -24,15 +24,24 @@ sap.ui.define([
 			var oOwnerComponent = this.getOwnerComponent();
 			this.oRouter = oOwnerComponent.getRouter();
 			this.oRouter.getRoute("materialIssueDetail").attachPatternMatched(this._onObjectMatched, this);
-			var oItemsModel = new JSONModel(sap.ui.require.toUrl("frontend/bbs/model/items.json"));
-			this.getView().setModel(oItemsModel,"items");
+			
 
         },
 		_onObjectMatched: async function (oEvent) {
+
+			this.getView().byId("materialIssueLineTableID").setBusy(true);
 			
 			var viewModel = new JSONModel();
 			this.getView().setModel(viewModel,"viewModel");
-			this.getView().byId("materialIssuePageId").setBusy(true);
+
+			var accountModel = new JSONModel();
+			this.getView().setModel(accountModel,"accounts");
+
+			var oItemsModel = new JSONModel();
+			oItemsModel.setSizeLimit(5000);
+			this.getView().setModel(oItemsModel,"items");
+
+			this.getView().byId("materialIssuePageID").setBusy(true);
 			var oStore = jQuery.sap.storage(jQuery.sap.storage.Type.local);
 			this.oJWT = oStore.get("jwt");
 
@@ -69,10 +78,11 @@ sap.ui.define([
 			this.getView().setModel(materialIssueDetailModel,"materialIssueDetailModel");
 
 			var oBudgetingModel = new JSONModel();
-				oBudgetingModel.loadData(backendUrl+"getBudget", null, true, "GET",false,false,{
+				oBudgetingModel.loadData(backendUrl+"budget/getApprovedBudget", null, true, "GET",false,false,{
 					'Authorization': 'Bearer ' + this.oJWT
 			});
-			this.getOwnerComponent().setModel(oBudgetingModel,"budgeting");
+			this.getView().setModel(oBudgetingModel,"budgeting");
+
 			materialIssueDetailModel.dataLoaded().then(function() { // Ensuring data availability instead of assuming it.
 				var userData = oUserModel.getData();
 				var materialIssueDetailData = this.getView().getModel("materialIssueDetailModel").getData();
@@ -106,12 +116,59 @@ sap.ui.define([
 					'Authorization': 'Bearer ' + this.oJWT
 				});
 				this.getOwnerComponent().setModel(oBudgetModel,"budget");
-				oBudgetModel.dataLoaded().then(function() {
-					this.getView().byId("materialIssuePageId").setBusy(false);
+				
+				accountModel.loadData(backendUrl+"coa/getCOAsByBudget?budgetCode="+materialIssueDetailData.U_BudgetCode, null, true, "GET",false,false,{
+					'Authorization': 'Bearer ' + this.oJWT
+				});
 
-				}.bind(this));
+				var materialIssueLineTable = this.getView().byId("materialIssueLineTableID");
+				var oItemsModel = this.getView().getModel("items");
+				oItemsModel.setProperty("/data", []);
+				var oItemData = oItemsModel.getData();
+				var itemsByAccount = new JSONModel();
+
+				var accounts = [];
+				for (let i = 0; i < materialIssueDetailData.MATERIALISSUELINESCollection.length; i++) {
+					if(!(materialIssueDetailData.MATERIALISSUELINESCollection[i].U_AccountCode.toString() in accounts)){
+						accounts.push(materialIssueDetailData.MATERIALISSUELINESCollection[i].U_AccountCode);
+					};
+				}
+				var uniqueAccounts = [... new Set(accounts)];
+				
+				for (let i = 0; i < uniqueAccounts.length; i++) {
+					this.getView().byId("materialIssuePageID").setBusy(true);
+
+					itemsByAccount.loadData(backendUrl+"items/getItemsByAccount?accountCode="+uniqueAccounts[i], null, true, "GET",false,false,{
+						'Authorization': 'Bearer ' + this.oJWT
+					});
+					itemsByAccount.dataLoaded().then(function(){
+						var itemsByAccountData = itemsByAccount.getData();
+						oItemData.data[uniqueAccounts[i]] = itemsByAccountData;
+						var newItemModel = new sap.ui.model.json.JSONModel(oItemData);
+						this.getView().setModel(newItemModel, 'items');
+						newItemModel.refresh();
+						this.getView().byId("materialIssuePageID").setBusy(false);
+					}.bind(this))
+				};
+
+				
+				for (let i = 0; i < materialIssueDetailData.MATERIALISSUELINESCollection.length; i++) {
+					materialIssueLineTable.getRows()[i].getCells()[1].setBusy(true);
+					let account = (materialIssueDetailData.MATERIALISSUELINESCollection[i].U_AccountCode).toString();
+					materialIssueLineTable.getRows()[i].getCells()[1].bindAggregation("items", {
+						path: 'items>/data/'+ account,
+						template: new sap.ui.core.Item({
+							key: "{items>ItemCode}",
+							text: "{items>ItemCode} - {items>ItemName}"
+						})
+					});
+					materialIssueLineTable.getRows()[i].getCells()[1].setBusy(false);
+				}
+
 				
 			}.bind(this));
+
+			this.getView().byId("materialIssueLineTableID").setBusy(false);
 			
 		},
 
@@ -128,7 +185,7 @@ sap.ui.define([
 		},
 
 		onApproveButtonClick : function(oEvent){
-			var pageDOM = this.getView().byId("materialIssuePageId");
+			var pageDOM = this.getView().byId("materialIssuePageID");
 			var viewModel = this.getView().getModel("viewModel");
 			pageDOM.setBusy(true);
 			var code = this.getView().byId("_IDGenText101").getText();
@@ -172,5 +229,141 @@ sap.ui.define([
 				}
 			  });
 		},
+
+		onSaveButtonClick : function(oEvent){
+			var pageDOM = this.getView().byId("materialIssuePageID");
+			pageDOM.setBusy(true);
+			var oModel = this.getView().getModel("materialIssueDetailModel");
+			var jsonData = JSON.stringify(oModel.getData());
+			var oJWT = this.oJWT;
+
+			$.ajax({
+				type: "POST",
+				data: jsonData,
+				headers: {"Authorization": "Bearer "+ oJWT},
+				crossDomain: true,
+				url: backendUrl+'materialIssue/saveMI',
+				contentType: "application/json",
+				success: function (res, status, xhr) {
+					  //success code
+					  pageDOM.setBusy(false);
+					  
+					  if (!this.oSuccessMessageDialog) {
+						this.oSuccessMessageDialog = new Dialog({
+							type: DialogType.Message,
+							title: "Success",
+							state: ValueState.Success,
+							content: new Text({ text: "Material Issue saved" }),
+							beginButton: new Button({
+								type: ButtonType.Emphasized,
+								text: "OK",
+								press: function () {
+									this.oSuccessMessageDialog.close();
+								}.bind(this)
+							})
+						});
+					}
+		
+					this.oSuccessMessageDialog.open();
+				},
+				error: function (jqXHR, textStatus, errorThrown) {
+				  	console.log("Got an error response: " + textStatus + errorThrown);
+				}
+			  });
+
+		},
+
+		onDelete: function(oEvent){
+
+			var row = oEvent.getParameters().row;
+			var iIdx = row.getIndex();
+			var oModel = this.getView().getModel("materialIssueDetailModel");
+			var oModelLineData = oModel.getData().MATERIALISSUELINESCollection;
+			oModelLineData.splice(iIdx, 1);
+			oModel.setProperty("/MATERIALISSUELINESCollection",oModelLineData);
+			oModel.refresh();
+		},
+		onBudgetChange : async function(oEvent){
+			var oValidatedComboBox = oEvent.getSource(),
+				sSelectedKey = oValidatedComboBox.getSelectedKey(),
+				sValue = oValidatedComboBox.getValue();
+
+			if (!sSelectedKey && sValue) {
+				oValidatedComboBox.setValueState(ValueState.Error);
+				oValidatedComboBox.setValueStateText("Please enter a valid Budget Code");
+			} else {
+				oValidatedComboBox.setValueState(ValueState.None);
+			}
+			if(oValidatedComboBox.getValueState() == ValueState.None){
+				this.getView().byId("materialIssuePageID").setBusy(true);
+
+				var accountModel = this.getView().getModel("accounts");
+				accountModel.loadData(backendUrl+"coa/getCOAsByBudget?budgetCode="+sSelectedKey, null, true, "GET",false,false,{
+					'Authorization': 'Bearer ' + this.oJWT
+				});
+				accountModel.refresh();
+
+				var oModel = this.getView().getModel("materialIssueDetailModel");
+				oModel.setProperty("/MATERIALISSUELINESCollection",[]);
+				oModel.refresh();
+				
+				var selectedID = parseInt(oEvent.getParameters('selectedItem').value);
+				var budget = this.getView().getModel("budget");
+				await budget.loadData(backendUrl+"budget/getBudgetById?code="+selectedID, null, true, "GET",false,false,{
+					'Authorization': 'Bearer ' + this.oJWT
+				});
+				budget.refresh();
+				
+				this.getView().byId("materialIssuePageID").setBusy(false);
+			}
+		},
+		onAddPress : function(oEvent){
+			const oModel = this.getView().getModel("materialIssueDetailModel");
+			var oModelData = oModel.getData();
+			var oNewObject = {
+				"U_AccountCode": "",
+				"U_ItemCode": "",
+				"U_Qty": ""
+			};
+			console.log(oModelData);
+			oModelData.MATERIALISSUELINESCollection.push(oNewObject);
+			var f = new sap.ui.model.json.JSONModel(oModelData);
+			this.getView().setModel(f, 'materialIssueDetailModel');
+			f.refresh();
+		
+		},
+
+		onAccountCodeChange : async function(oEvent){
+			
+			var oSelectedItem = oEvent.getSource().getSelectedKey(); //Get Selected Item
+			var oSelectedRow = oEvent.getSource().getParent(); //Selected Row.
+			oSelectedRow.getCells()[1].setBusy(true);
+			oSelectedRow.getCells()[1].setSelectedKey("");
+			oSelectedRow.getCells()[1].setEnabled(true);
+			oSelectedRow.getCells()[1].setEnabled(true);
+
+			var oItemModel = this.getView().getModel("items");
+			var oItemData = oItemModel.getData();
+			if(!(oSelectedItem.toString() in oItemData)){
+				var oItemByAccountModel = new JSONModel();
+				await oItemByAccountModel.loadData(backendUrl+"items/getItemsByAccount?accountCode="+oSelectedItem+"", null, true, "GET",false,false,{
+					'Authorization': 'Bearer ' + this.oJWT
+				});
+				var oItemByAccountData = oItemByAccountModel.getData();
+				oItemData[oSelectedItem] = oItemByAccountData;
+				var i = new sap.ui.model.json.JSONModel(oItemData);
+				this.getView().setModel(i, 'items');
+				i.refresh();
+			}
+
+			oSelectedRow.getCells()[1].bindAggregation("items", {
+				path: 'items>/'+ oSelectedItem,
+				template: new sap.ui.core.Item({
+					key: "{items>ItemCode}",
+					text: "{items>ItemCode} - {items>ItemName}"
+				})
+			});
+			oSelectedRow.getCells()[1].setBusy(false);
+		}
 	});
 });

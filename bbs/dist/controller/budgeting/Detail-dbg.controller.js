e2fbd46193e0f1fc159634db244d82879f435c73
Deril Alwinda <deril.alwinda@harrisma.com>
2023-08-14 13:32:32 +0700

    Budgeting fixes

--- bbs/dist/controller/budgeting/Detail-dbg.controller.js
+++ bbs/dist/controller/budgeting/Detail-dbg.controller.js
@@ -503,5 +503,32 @@ sap.ui.define([
 			oModel.refresh();
 			this.onAmountChange();
 		},
+
+		
+		objectFormatter: function(sStatus) {
+			if(sStatus == 1 ){
+				return 'Warning';
+			}else if(sStatus == 2){
+				return 'Information';
+			}
+			else if(sStatus == 3){
+				return 'Success';
+			}else{
+				return 'Error';
+			}
+		  },
+		
+		textFormatter : function(sStatus){
+			if(sStatus == 1){
+				return 'Pending'
+			}else if(sStatus == 2){
+				return 'Approved by Manager'
+			}else if(sStatus == 3){
+				return 'Approved by Director'
+			}else{
+				return 'Rejected'
+			}
+		  
+		}
 	});
 });

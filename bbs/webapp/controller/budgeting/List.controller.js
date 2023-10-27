6a89e6ae1d60a4aa72164901683c0eeacb1448da
Deril Alwinda <deril.alwinda@harrisma.com>
2023-10-27 11:24:14 +0700

    change error dialog to massge dialog

--- bbs/webapp/controller/budgeting/List.controller.js
+++ bbs/webapp/controller/budgeting/List.controller.js
@@ -264,22 +264,8 @@ sap.ui.define([
 						},
 						error: function (jqXHR, textStatus, errorThrown) {
 							oDialog.setBusy(false);
-							if (!this.oErrorDialog) {
-								this.oErrorDialog = new Dialog({
-									type: DialogType.Message,
-									title: "Error",
-									state: ValueState.Error,
-									content: new Text({ text: jqXHR.responseJSON.msg }),
-									beginButton: new Button({
-										type: ButtonType.Emphasized,
-										text: "OK",
-										press: function () {
-											this.oErrorDialog.close();
-										}.bind(this)
-									})
-								});
-							};
-							this.oErrorDialog.open();
+							MessageBox.error(jqXHR.responseJSON.msg);
+							
 						}
 					});
 
